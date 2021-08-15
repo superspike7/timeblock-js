1684a3cc6eb3989fc41b9719008783a607343703
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-15 14:05:14 +0800

    add timblock constructor

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function TimeBlock(options){
+
+    this.wakingHours = options.wakingHours || "05:00AM";
+    this.sleepingHours = options.sleepingHours || "09:00PM";
+
+};
+
+
+
+
+console.log(normalDay);
