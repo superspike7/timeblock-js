5d076cd5de839b1f605bfa90fc17669cd6029c25
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-16 08:26:03 +0800

    add timeblock Component

--- src/index.js
+++ src/index.js
@@ -23,3 +23,21 @@ TimeBlock.prototype.range = function() {
 var test = new TimeBlock({});
 console.log(test.range());
 
+const TimeBlockComponent = () => {
+  const grid = document.createElement('div');
+  const time = document.createElement('div');
+
+  grid.classList.add('grid', 'lg:grid-cols-4', 'lg:w-1/2', 'mx-auto', 'bg-gray-200');
+  time.classList.add('bg-blue-500', 'text-white', 'col-span-2', 'row-start-1');
+
+  const setRowTemplate = n => { grid.classList.add(`lg:grid-rows-${n * 2}`)};
+
+  return {
+    grid,
+    setRowTemplate
+  };
+
+};
+
+document.body.appendChild(TimeBlockComponent.grid);
+TimeBlockComponent.setRowTemplate(test.range().length);
