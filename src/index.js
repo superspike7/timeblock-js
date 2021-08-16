7c2d4fc293552d0cac2804072914ec7d0eae3a5d
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-16 09:59:46 +0800

    style timeblock componenet

--- src/index.js
+++ src/index.js
@@ -2,63 +2,65 @@ import "./style.css"
 
 "use strict";
 
-function TimeBlock(options){
+// function TimeBlock(options){
 
-    this.wakingHour = options.wakingHour || 5;
-    this.sleepingHour = options.sleepingHour || 21;
+//     this.wakingHour = options.wakingHour || 5;
+//     this.sleepingHour = options.sleepingHour || 21;
 
-};
-
-TimeBlock.prototype.range = function() {
-      let first = Number(this.wakingHour);
-      let last = Number(this.sleepingHour);
-      let arr = [];
-      for(let i = first; i <= last; i++){
-        arr.push(i);
-      }
-      return arr;
-};
-
-
-TimeBlock.prototype.component = function() {
-  return timeBlockComponent(this.range());
-}
-
-// const timeBlock = (wakeTime, sleepTime) => {
-
-//   const range = () => {
-//     let arr = [];
-//     for(let i = Number(wakeTime); i <= Number(sleepTime); i++) {
-//       arr.push(i);
-//     }
-//     return arr;
-//   };
-
-//   const component = () => { 
-//    console.log(TimeBlockComponent(range())) ;
-
-//   };
-
-//   return {
-//     range,
-//     component
-//   };
-  
 // };
+
+// TimeBlock.prototype.range = function() {
+//       let first = Number(this.wakingHour);
+//       let last = Number(this.sleepingHour);
+//       let arr = [];
+//       for(let i = first; i <= last; i++){
+//         arr.push(i);
+//       }
+//       return arr;
+// };
+
+
+// TimeBlock.prototype.component = function() {
+//   return timeBlockComponent(this.range());
+// }
+
+const timeBlock = (wakeTime, sleepTime) => {
+
+  const range = () => {
+    let arr = [];
+    for(let i = Number(wakeTime); i <= Number(sleepTime); i++) {
+      arr.push(i);
+    }
+    return arr;
+  };
+
+  const component = () => { 
+    return timeBlockComponent(range());
+  };
+
+  return {
+    component
+  };
+  
+};
 
 
 const timeBlockComponent = (range) => {
   const grid = document.createElement('div');
 
   const setRowTemplate = () => { 
-    grid.classList.add('grid', 'lg:grid-cols-4', 'lg:w-1/2', 'mx-auto', 'bg-gray-200');
+    grid.classList.add('grid', 'gap-px', 'lg:grid-cols-4', 'mx-auto', 'bg-gray-200');
   }; 
 
   const setTimes = () => {
     range.forEach( n => {
       const time = document.createElement('div');
       time.innerHTML = n;
-      time.classList.add('bg-blue-500', 'text-white', 'col-span-1', 'col-start-1', 'h-20');
+      time.classList.add('bg-blue-500',
+                         'text-white', 'col-span-1',
+                         'col-start-1', 'h-32', 'flex',
+                         'items-center', 'justify-center',
+                         'w-32', 'text-5xl');
       grid.appendChild(time);
     });
   }; 
@@ -73,5 +75,6 @@ const timeBlockComponent = (range) => {
 
 };
 
-var test = new TimeBlock({});
-document.body.appendChild(test.component().grid);
+var test = timeBlock(4, 21);
+document.querySelector('.block').appendChild(test.component().grid);
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      spacing: {
+        '80v' : '80vh'
+      },
     },
   },
   variants: {
