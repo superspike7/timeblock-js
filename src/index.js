f079245e55921a06ba55308a87bfaa67923d97a8
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-17 07:30:52 +0800

    add taskComponent

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const timeBlockComponent = (range) => {
   const grid = document.createElement('div');
 
   const setRowTemplate = () => { 
-    grid.classList.add('grid', 'gap-px', 'lg:grid-cols-4', 'mx-auto', 'bg-gray-200');
+    grid.classList.add('grid', 'gap-px', 'grid-cols-1', 'bg-gray-200');
   }; 
 
   const setTimes = () => {
@@ -36,9 +36,9 @@ const timeBlockComponent = (range) => {
       time.innerHTML = n;
       time.classList.add('bg-blue-500',
                          'text-white', 'col-span-1',
-                         'col-start-1', 'h-32', 'flex',
+                         'col-span-1', 'h-56', 'flex',
                          'items-center', 'justify-center',
-                         'w-32', 'text-5xl');
+                         'text-5xl');
       grid.appendChild(time);
     });
   }; 
@@ -53,6 +53,28 @@ const timeBlockComponent = (range) => {
 
 };
 
+const taskComponent = () => {
+  const taskGrid = document.createElement('div');
+  taskGrid.classList.add('bg-gray-400','border-blue-500',
+                         'h-10', 'col-start-2', 'col-span-3',
+                         'gap-px', 'grid');
+
+  const createTask = (height, prio) => {
+    const task = document.createElement('div');
+    task.classList.add('bg-blue-300');
+    task.classList.add(`h-${height}`, `col-span-${prio}`)
+    taskGrid.appendChild(task);
+  }
+
+  createTask("52");
+
+  return {
+    taskGrid
+  };
+
+};
+
 var test = timeBlock(4, 21);
-document.querySelector('.block').appendChild(test.component().grid);
+document.querySelector('.main-grid').appendChild(test.component().grid);
+document.querySelector('.main-grid').appendChild(taskComponent().taskGrid);
 
