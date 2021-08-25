79d93a6947e78f2160f3c27312f5f422c52fff16
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-25 09:57:46 +0800

    add task buttons

--- src/taskComponent.js
+++ src/taskComponent.js
@@ -4,8 +4,20 @@ const taskComponent = (obj) => {
     const task = document.createElement('div');
     const title = document.createElement('h1');
     const description = document.createElement('p');
-    task.classList.add('bg-blue-100', 'flex', 'flex-col', 'justify-center', 'items-center', 'py-1', 'text-center');
+    const removeBtn = document.createElement('span');
+    const editBtn = document.createElement('span');
+    const checkBtn = document.createElement('span');
+    const btns = document.createElement('div');
+    task.classList.add('bg-blue-100', 'flex', 'flex-col', 'justify-center', 'items-center', 'py-1', 'text-center', 'task-container', 'relative');
     task.classList.add(`h-${obj.time}`, `w-${obj.type}`);
+    btns.classList.add('absolute', 'top-px', 'right-px', 'inline-flex', 'space-x-1');
+    removeBtn.classList.add('p-1', 'text-red-500', 'text-white', 'cursor-pointer', 'hover:bg-red-800', 'hover:text-white');
+    editBtn.classList.add('p-1', 'text-blue-500', 'text-white', 'cursor-pointer', 'hover:bg-blue-800', 'hover:text-white');
+    checkBtn.classList.add('p-1', 'text-green-500', 'text-white', 'cursor-pointer', 'hover:bg-green-800', 'hover:text-white');
+
+    removeBtn.innerHTML = "X";
+    editBtn.innerHTML = "Edit";
+    checkBtn.innerHTML = "Done";
 
     title.classList.add('text-xl', 'font-semibold', 'text-gray-900', 'shadow-sm');
 
@@ -14,6 +26,10 @@ const taskComponent = (obj) => {
     title.textContent = obj.title;
     description.textContent = obj.description;
 
+    btns.appendChild(checkBtn);
+    btns.appendChild(editBtn);
+    btns.appendChild(removeBtn);
+    task.appendChild(btns)
     task.appendChild(title);
     task.appendChild(description);
 
